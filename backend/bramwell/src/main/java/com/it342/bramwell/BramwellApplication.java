b36448e4d187f293e563ad507071166358c74ea4
LAB13_USER ON WS20 <L13Y12W20@LabHCI.net>
2026-02-07 12:02:24 +0800

    First commit: initial setup of project

---
+++ backend/bramwell/src/main/java/com/it342/bramwell/BramwellApplication.java
@@ -0,0 +1,13 @@
+package com.it342.bramwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BramwellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BramwellApplication.class, args);
+	}
+
+}
---
+++ backend/bramwell/src/test/java/com/it342/bramwell/BramwellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it342.bramwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BramwellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
